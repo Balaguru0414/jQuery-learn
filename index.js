b2f87f48ef7cb47e0950bbafd4b7380cb7fbe960
L.Balaguru <117264451+Balaguru0414@users.noreply.github.com>
2022-12-10 22:03:36 +0530

    jQuery

---
+++ index.js
@@ -0,0 +1,51 @@
+$("h1").on("mouseover",function () {
+	$("h1").addClass("big-head");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// addeventlistener -- keypress chage text
+
+// $(document).keypress(function (event) {
+// 	$("h1").text(event.key);
+// });
+
+
+// jQuery -- addeventlistener 
+
+// $("button").click(function () {
+//     $("h1").text("I am your Best Friend");
+// 		$("h1").addClass("big-head");
+// });
+
+// DOM -- addeventlistener -- with -- if else
+
+
+
+// 	for (let i = 0; i < 6; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click",function () {if (i%2===0) {
+//         document.querySelector("h1").classList.add("big-head");document.querySelector("h1").innerHTML="Surya";
+//     } else {
+//       document.querySelector("h1").classList.add("big-head");document.querySelector("h1").innerHTML="Suppi";  
+//     }
+        
+//     });
+    
+// }
